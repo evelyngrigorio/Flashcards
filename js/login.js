6b02483e17a4916a8e95dc0e0f9dd623ed422f44
evelyngrigorio <evelyngrigorioaltier@gmail.com>
2023-02-16 02:38:05 -0300

    incorporando oauth2 de google en el login

---
+++ js/login.js
@@ -0,0 +1,32 @@
+
+
+/*function signIn(){
+    // Google's OAuth 2.0 endpoint for requesting an access token
+  var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+  // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+  var form = document.createElement('form');
+  form.setAttribute('method', 'GET'); // Send as a GET request.
+  form.setAttribute('action', oauth2Endpoint);
+
+  // Parameters to pass to OAuth 2.0 endpoint.
+  var params = {'client_id': '977601273994-plp29uh10pu3h45f6udldm7tjp0j5obm.apps.googleusercontent.com',
+                'redirect_uri': 'homepage.html',
+                'response_type': 'token',
+                'scope':'https://www.googleapis.com/auth/userinfo.profile',
+                'include_granted_scopes': 'true',
+                'state': 'pass-through value'};
+
+  // Add form parameters as hidden input values.
+  for (var p in params) {
+    var input = document.createElement('input');
+    input.setAttribute('type', 'hidden');
+    input.setAttribute('name', p);
+    input.setAttribute('value', params[p]);
+    form.appendChild(input);
+  }
+
+  // Add form to page and submit it to open the OAuth 2.0 endpoint.
+  document.body.appendChild(form);
+  form.submit();
+}*/
